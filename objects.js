5cfbb971d54e8df17f759ba4fc1d221e8f37bcc8
Daniel Zaltsman <daniel.zaltsman92@gmail.com>
2019-08-04 05:13:21 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,6 +3,7 @@ var playlist = {michael_jackson : 'beat it' }
 function updatePlaylist(obj, artist, song){
   
   Object.assign({}, obj, {[artist]:song});
+  console.log(playlist)
   return playlist;
   
 }
